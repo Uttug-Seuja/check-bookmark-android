caa86d40103d38551fbd8659cdde56c47f45223c
JunJangE <wnswkd486@gmail.com>
2023-03-08 19:22:03 +0900

    [CheckBookmark] main > 바텀 네비게이션

---
+++ app/src/main/java/com/o2/check_bookmark_android/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.o2.check_bookmark_android.ui
+
+import com.ao2.run_eat.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
+
+    private val TAG = "MainViewModel"
+}
