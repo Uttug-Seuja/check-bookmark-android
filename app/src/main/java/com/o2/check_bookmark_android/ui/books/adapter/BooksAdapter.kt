804870d01c2823a5957cff71d8ad980ad13a4837
JunJangE <wnswkd486@gmail.com>
2023-03-11 16:12:53 +0900

    [CheckBookmark] 서재 >  BooksAdapter 추가

---
+++ app/src/main/java/com/o2/check_bookmark_android/ui/books/adapter/BooksAdapter.kt
@@ -0,0 +1,40 @@
+package com.o2.check_bookmark_android.ui.books.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.o2.check_bookmark_android.databinding.HolderBookBinding
+import com.o2.check_bookmark_android.databinding.HolderBookmarkStackBinding
+import com.o2.check_bookmark_android.ui.books.BooksActionHandler
+import com.o2.check_bookmark_android.ui.home.HomeActionHandler
+import com.o2.domain.model.Book
+import com.o2.domain.model.BookmarkStack
+import com.o2.domain.model.Books
+
+class BooksAdapter(
+    private val eventListener: BooksActionHandler,
+) : ListAdapter<Book, BooksViewHolder>(RecommendationItemDiffCallback){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        return BooksViewHolder(
+            HolderBookBinding.inflate(LayoutInflater.from(parent.context), parent, false).apply {
+                eventListener = this@BooksAdapter.eventListener
+            }
+        )
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        getItem(position)?.let { item ->
+            holder.bind(item)
+        }
+    }
+
+    internal object RecommendationItemDiffCallback : DiffUtil.ItemCallback<Book>() {
+        override fun areItemsTheSame(oldItem: Book, newItem: Book) =
+            oldItem == newItem
+
+        override fun areContentsTheSame(oldItem: Book, newItem: Book) =
+            oldItem.equals(newItem)
+    }
+}
---
+++ app/src/main/java/com/o2/check_bookmark_android/ui/books/adapter/BooksViewHolder.kt
@@ -0,0 +1,20 @@
+package com.o2.check_bookmark_android.ui.books.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.o2.check_bookmark_android.databinding.HolderBookBinding
+import com.o2.check_bookmark_android.databinding.HolderBookmarkStackBinding
+import com.o2.domain.model.Book
+import com.o2.domain.model.BookmarkStack
+import com.o2.domain.model.Books
+
+class BooksViewHolder(
+    private val binding: HolderBookBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: Book) {
+        binding.apply {
+            holder = item
+            executePendingBindings()
+        }
+    }
+}
