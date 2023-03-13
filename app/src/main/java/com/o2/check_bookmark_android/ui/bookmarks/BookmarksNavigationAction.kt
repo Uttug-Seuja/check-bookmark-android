9b9fdd614648eb6452ddbc0fd17e03b910775080
junjange <wnswkd486@naver.com>
2023-03-13 16:37:10 +0900

    [CheckBookmark] 책갈피 페이지 > 책갈피 상세 페이지로 이동

--- app/src/main/java/com/o2/check_bookmark_android/ui/bookmarks/BookmarksNavigationAction.kt
+++ app/src/main/java/com/o2/check_bookmark_android/ui/bookmarks/BookmarksNavigationAction.kt
@@ -2,5 +2,6 @@ package com.o2.check_bookmark_android.ui.bookmarks
 
 sealed class BookmarksNavigationAction {
     object NavigateToBookmarkCreate: BookmarksNavigationAction()
+    object NavigateToBookmarkDetail: BookmarksNavigationAction()
     object NavigateToBookmark: BookmarksNavigationAction()
 }
