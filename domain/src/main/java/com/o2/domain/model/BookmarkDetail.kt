bbef3905f901f140b8183e5b5184b36a88a92615
JunJangE <wnswkd486@gmail.com>
2023-03-16 10:43:53 +0900

    [CheckBookmark] model >  BookmarkDetail 추가

---
+++ domain/src/main/java/com/o2/domain/model/BookmarkDetail.kt
@@ -0,0 +1,11 @@
+package com.o2.domain.model
+
+data class BookmarkDetail(
+    val book_name: String,
+    val title: String,
+    val emotion: String,
+    val description: String,
+    val last: Int,
+    val date: String,
+    val bookmark_color: String
+)
