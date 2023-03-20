ce6ffc98ab37e000af2934830532f26de3c2b9fc
JunJangE <wnswkd486@gmail.com>
2023-03-20 10:21:28 +0900

    [CheckBookmark]다이얼로그 > dialog_yellow_default_alert 추가

--- app/src/main/java/com/o2/check_bookmark_android/base/BaseAlertDialog.kt
+++ app/src/main/java/com/o2/check_bookmark_android/base/BaseAlertDialog.kt
@@ -1,8 +1,10 @@
 package com.o2.check_bookmark_android.base
 
+import android.view.View
 import com.o2.check_bookmark_android.R
 import com.o2.check_bookmark_android.databinding.DialogGrayDefaultAlertBinding
 import com.o2.check_bookmark_android.databinding.DialogRedDefaultAlertBinding
+import com.o2.check_bookmark_android.databinding.DialogYellowDefaultAlertBinding
 
 
 data class AlertDialogModel(
@@ -11,6 +13,40 @@ data class AlertDialogModel(
     val negativeContents : String,
     val positiveContents : String
 )
+
+class DefaultYellowAlertDialog(
+    private val alertDialogModel: AlertDialogModel,
+    private val clickToNegative: () -> Unit,
+    private val clickToPositive: () -> Unit,
+) : BaseDialog<DialogYellowDefaultAlertBinding>(layoutId = R.layout.dialog_yellow_default_alert) {
+
+    override val layoutResourceId: Int
+        get() = R.layout.dialog_yellow_default_alert
+
+    override fun initStartView() {
+        binding.model = alertDialogModel
+    }
+
+    override fun initDataBinding() = Unit
+
+    override fun initAfterBinding() {
+        if(alertDialogModel.title == null) {
+            binding.alertTitle.visibility = View.GONE
+        }
+        if(alertDialogModel.description == null) {
+            binding.alertDescription.visibility = View.GONE
+        }
+
+        binding.negativeBtn.setOnClickListener {
+            clickToNegative.invoke()
+            dismiss()
+        }
+        binding.positiveBtn.setOnClickListener {
+            clickToPositive.invoke()
+            dismiss()
+        }
+    }
+}
 
 class DefaultRedAlertDialog(
     private val alertDialogModel: AlertDialogModel,
