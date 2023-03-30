c8abca2c4a3e452108c416d897940762b9295ae8
junjange <wnswkd486@naver.com>
2023-03-31 00:06:14 +0900

    [CheckBookmark] Network > Repository 연결 및 서버 통신

--- data/src/main/java/com/o2/data/repository/MainRepositoryImpl.kt
+++ data/src/main/java/com/o2/data/repository/MainRepositoryImpl.kt
@@ -5,8 +5,7 @@ import com.o2.data.api.MainAPIService
 import com.o2.data.api.handleApi
 import com.o2.data.model.request.*
 import com.o2.domain.NetworkResult
-import com.o2.domain.model.ImageUrl
-import com.o2.domain.model.LoginResponse
+import com.o2.domain.model.*
 import com.o2.domain.repository.MainRepository
 import okhttp3.MultipartBody
 import javax.inject.Inject
@@ -18,6 +17,140 @@ class MainRepositoryImpl @Inject constructor(
 
     override suspend fun postFileToUrl(file: MultipartBody.Part): NetworkResult<ImageUrl> {
         return handleApi { mainAPIService.postFileToUrl(file = file).data }
+    }
+
+    override suspend fun postLogin(idTokenString: String): NetworkResult<Unit> {
+        return handleApi { mainAPIService.postLogin(idTokenString = idTokenString) }
+    }
+
+    override suspend fun postSignUp(
+        idToken: String,
+        nickname: String,
+        profile_path: String
+    ): NetworkResult<Unit> {
+        val body = PostSignUpRequest(nickname, profile_path)
+        return handleApi { mainAPIService.postSignUp(body = body) }
+    }
+
+    override suspend fun postLogOut(): NetworkResult<Unit> {
+        return handleApi { mainAPIService.postLogOut() }
+    }
+
+    override suspend fun getUserProfile(): NetworkResult<UserProfile> {
+        return handleApi { mainAPIService.getUserProfile().data }
+    }
+
+    override suspend fun postUserProfile(
+        nickname: String,
+        profile_path: String
+    ): NetworkResult<Unit> {
+        val body = PostUserProfileRequest(nickname, profile_path)
+        return handleApi { mainAPIService.postUserProfile(body = body) }
+    }
+
+    override suspend fun getRandomProfileImage(): NetworkResult<ImageUrl> {
+        return handleApi { mainAPIService.getRandomProfileImage().data }
+    }
+
+    override suspend fun getMoodImage(): NetworkResult<MoodImageUrl> {
+        return handleApi { mainAPIService.getMoodImage().data }
+    }
+
+    override suspend fun postBooks(
+        bookName: String,
+        author: String,
+        publisher: String,
+        pageNumber: String
+    ): NetworkResult<Books> {
+        val body = PostBooksRequest(bookName, author, publisher, pageNumber)
+        return handleApi { mainAPIService.postBooks(body = body).data }
+    }
+
+    override suspend fun deleteBooks(bookId: Int): NetworkResult<Unit> {
+        return handleApi { mainAPIService.deleteBooks(bookId = bookId) }
+    }
+
+    override suspend fun updateBooks(
+        bookName: String,
+        author: String,
+        publisher: String,
+        pageNumber: String
+    ): NetworkResult<Books> {
+        val body = PostBooksRequest(bookName, author, publisher, pageNumber)
+        return handleApi { mainAPIService.updateBooks(body = body).data }
+    }
+
+    override suspend fun getBooksMy(): NetworkResult<BooksMy> {
+        return handleApi { mainAPIService.getBooksMy().data }
+    }
+
+    override suspend fun getBooksHome(): NetworkResult<BooksHome> {
+        return handleApi { mainAPIService.getBooksHome().data }
+    }
+
+    override suspend fun getBooksSummary(bookId: Int): NetworkResult<BooksSummary> {
+        return handleApi { mainAPIService.getBooksSummary(bookId = bookId).data }
+    }
+
+    override suspend fun getBooksClub(body: Int): NetworkResult<BooksClub> {
+        return handleApi { mainAPIService.getBooksClub(body = body).data }
+    }
+
+    override suspend fun getBooksClubSummary(bookId: Int): NetworkResult<BooksSummary> {
+        return handleApi { mainAPIService.getBooksClubSummary(bookId = bookId).data }
+    }
+
+    override suspend fun postBooksResister(bookId: Int): NetworkResult<Unit> {
+        return handleApi { mainAPIService.postBooksResister(bookId = bookId) }
+    }
+
+    override suspend fun postBooksResisterCancel(bookId: Int): NetworkResult<Unit> {
+        return handleApi { mainAPIService.postBooksResisterCancel(bookId = bookId) }
+    }
+
+    override suspend fun postBooksLike(bookId: Int): NetworkResult<Unit> {
+        return handleApi { mainAPIService.postBooksLike(bookId = bookId) }
+    }
+
+    override suspend fun postBookmark(
+        bookId: Int,
+        bookMarkName: String,
+        moodImageUrl: String,
+        summary: String,
+        checkPageNum: Int,
+        color: String
+    ): NetworkResult<Bookmark> {
+        val body = PostBookmarkRequest(bookMarkName, moodImageUrl, summary, checkPageNum, color)
+        return handleApi { mainAPIService.postBookmark(bookId = bookId, body = body).data }
+    }
+
+    override suspend fun deleteBookmark(bookMarkId: Int): NetworkResult<Unit> {
+        return handleApi { mainAPIService.deleteBookmark(bookMarkId = bookMarkId) }
+    }
+
+    override suspend fun updateBookmark(
+        bookMarkId: Int,
+        bookMarkName: String,
+        moodImageUrl: String,
+        summary: String,
+        checkPageNum: Int,
+        color: String
+    ): NetworkResult<Bookmark> {
+        val body = PostBookmarkRequest(bookMarkName, moodImageUrl, summary, checkPageNum, color)
+        return handleApi {
+            mainAPIService.updateBookmark(
+                bookMarkId = bookMarkId,
+                body = body
+            ).data
+        }
+    }
+
+    override suspend fun getBookmark(bookId: Int, page: Int): NetworkResult<PagingBookmarkList> {
+        return handleApi { mainAPIService.getBookmark(bookId = bookId, page = page).data }
+    }
+
+    override suspend fun getBookmarkDetail(bookMarkId: Int): NetworkResult<BookmarkDetail> {
+        return handleApi { mainAPIService.getBookmarkDetail(bookMarkId = bookMarkId).data }
     }
 
     override suspend fun postRefreshToken(refresh_token: String): NetworkResult<LoginResponse> {
--- domain/src/main/java/com/o2/domain/repository/MainRepository.kt
+++ domain/src/main/java/com/o2/domain/repository/MainRepository.kt
@@ -1,9 +1,10 @@
 package com.o2.domain.repository
 
+import com.google.gson.annotations.SerializedName
 import com.o2.domain.NetworkResult
-import com.o2.domain.model.ImageUrl
-import com.o2.domain.model.LoginResponse
+import com.o2.domain.model.*
 import okhttp3.MultipartBody
+import retrofit2.http.*
 
 interface MainRepository {
 
@@ -12,6 +13,130 @@ interface MainRepository {
 
     // 파일 URL로 바꾸기
     suspend fun postFileToUrl(
-        file : MultipartBody.Part
+        file: MultipartBody.Part
     ): NetworkResult<ImageUrl>
+
+    // 로그인(회원인지 판단)
+    suspend fun postLogin(
+        idTokenString: String
+    ): NetworkResult<Unit>
+
+    // 회원가입
+    suspend fun postSignUp(
+        idToken: String,
+        nickname: String,
+        profile_path: String
+    ): NetworkResult<Unit>
+
+    // 로그아웃
+    suspend fun postLogOut(): NetworkResult<Unit>
+
+    // 유저 프로필
+    suspend fun getUserProfile(): NetworkResult<UserProfile>
+
+    // 유저 프로필 변경
+    suspend fun postUserProfile(
+        nickname: String,
+        profile_path: String
+    ): NetworkResult<Unit>
+
+    // 랜덤 기본 프로필 이미지 가져오기
+    suspend fun getRandomProfileImage(): NetworkResult<ImageUrl>
+
+    // 기분 이미지 가져오기
+    suspend fun getMoodImage(): NetworkResult<MoodImageUrl>
+
+    // 책 생성
+    suspend fun postBooks(
+        bookName: String,
+        author: String,
+        publisher: String,
+        pageNumber: String
+    ): NetworkResult<Books>
+
+    // 책 삭제
+    suspend fun deleteBooks(
+        bookId: Int
+    ): NetworkResult<Unit>
+
+    // 책 수정
+    suspend fun updateBooks(
+        bookName: String,
+        author: String,
+        publisher: String,
+        pageNumber: String
+    ): NetworkResult<Books>
+
+    // 내가 읽은 책 List 보여주기
+    suspend fun getBooksMy(): NetworkResult<BooksMy>
+
+    // 나의 홈화면 책 리스트
+    suspend fun getBooksHome(): NetworkResult<BooksHome>
+
+    // 나만의 책 요약
+    suspend fun getBooksSummary(
+        bookId: Int
+    ): NetworkResult<BooksSummary>
+
+    // 북클럽 리스트
+    suspend fun getBooksClub(
+        body: Int
+    ): NetworkResult<BooksClub>
+
+    // 북클럽 책 요약
+    suspend fun getBooksClubSummary(
+        bookId: Int
+    ): NetworkResult<BooksSummary>
+
+    // 책 자랑하기 등록
+    suspend fun postBooksResister(
+        bookId: Int
+    ): NetworkResult<Unit>
+
+    // 책 자랑하기 등록 취소
+    suspend fun postBooksResisterCancel(
+        bookId: Int
+    ): NetworkResult<Unit>
+
+    // 책 좋아요
+    suspend fun postBooksLike(
+        bookId: Int
+    ): NetworkResult<Unit>
+
+    // 북마크 생성
+    suspend fun postBookmark(
+        bookId: Int,
+        bookMarkName: String,
+        moodImageUrl: String,
+        summary: String,
+        checkPageNum: Int,
+        color: String
+    ): NetworkResult<Bookmark>
+
+    // 북마크 삭제
+    suspend fun deleteBookmark(
+        bookMarkId: Int,
+    ): NetworkResult<Unit>
+
+    // 북마크 업데이트
+    suspend fun updateBookmark(
+        bookMarkId: Int,
+        bookMarkName: String,
+        moodImageUrl: String,
+        summary: String,
+        checkPageNum: Int,
+        color: String
+    ): NetworkResult<Bookmark>
+
+    // 북마크 리스트 페이징
+    suspend fun getBookmark(
+        bookId: Int,
+        page: Int
+    ): NetworkResult<PagingBookmarkList>
+
+    // 북마크 세부 정보
+    suspend fun getBookmarkDetail(
+        bookMarkId: Int,
+    ): NetworkResult<BookmarkDetail>
+
 }
