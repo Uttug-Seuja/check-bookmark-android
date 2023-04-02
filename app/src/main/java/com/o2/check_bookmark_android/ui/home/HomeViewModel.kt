f69d4eeafd1d854d549c8f47984bc6e551e69fbe
junjange <wnswkd486@naver.com>
2023-04-02 13:14:43 +0900

    [CheckBookmark] 홈 페이지 > 나의 홈화면 책 리스트 API 연결

--- app/src/main/java/com/o2/check_bookmark_android/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/o2/check_bookmark_android/ui/home/HomeViewModel.kt
@@ -1,9 +1,10 @@
 package com.o2.check_bookmark_android.ui.home
 
 import com.o2.check_bookmark_android.base.BaseViewModel
-import com.o2.domain.model.BookHomeItem
-import com.o2.domain.model.BookHome
 import com.o2.domain.model.BooksHome
+import com.o2.domain.onError
+import com.o2.domain.onSuccess
+import com.o2.domain.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,6 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
+    private val mainRepository: MainRepository
 ) : BaseViewModel(), HomeActionHandler {
 
     private val TAG = "HomeViewModel"
@@ -22,53 +24,14 @@ class HomeViewModel @Inject constructor(
     val bookmarkStackEvent: StateFlow<BooksHome> = _bookmarkStackEvent
 
     init {
-        getTempList()
-    }
-
-    private fun getTempList() {
-        val test1 = BookHome(
-            listOf(
-                BookHomeItem("어린왕자(생택취페리 탄생 120주년 블라블라)", 1),
-                BookHomeItem("붕대 감기(윤이형 소설)", 1),
-                BookHomeItem("초록빛 힐링의 섬 아일랜드에 멈추다 하하하하", 1),
-                BookHomeItem("호모데우스(미래의 역사)", 1)
-            )
-        )
-        val test2 = BookHome(
-            listOf(
-                BookHomeItem("어린왕자(생택취페리 탄생 120주년 블라블라)", 1),
-                BookHomeItem("붕대 감기(윤이형 소설)", 1),
-                BookHomeItem("초록빛 힐링의 섬 아일랜드에 멈추다 하하하하", 1),
-                BookHomeItem("호모데우스(미래의 역사)", 1)
-            )
-        )
-        val test3 = BookHome(
-            listOf(
-                BookHomeItem("어린왕자(생택취페리 탄생 120주년 블라블라)", 1),
-                BookHomeItem("붕대 감기(윤이형 소설)", 1),
-                BookHomeItem("초록빛 힐링의 섬 아일랜드에 멈추다 하하하하", 1),
-                BookHomeItem("호모데우스(미래의 역사)", 1)
-            )
-        )
-        val test4 = BookHome(
-            listOf(
-                BookHomeItem("어린왕자(생택취페리 탄생 120주년 블라블라)", 1),
-                BookHomeItem("붕대 감기(윤이형 소설)", 1),
-                BookHomeItem("초록빛 힐링의 섬 아일랜드에 멈추다 하하하하", 1),
-                BookHomeItem("호모데우스(미래의 역사)", 1)
-            )
-        )
-        val test5 = BookHome(
-            listOf(
-                BookHomeItem("어린왕자(생택취페리 탄생 120주년 블라블라)", 1),
-                BookHomeItem("붕대 감기(윤이형 소설)", 1),
-                BookHomeItem("초록빛 힐링의 섬 아일랜드에 멈추다 하하하하", 1),
-                BookHomeItem("호모데우스(미래의 역사)", 1)
-            )
-        )
-        val testList = BooksHome(listOf(test1, test2, test3, test4, test5))
         baseViewModelScope.launch {
-            _bookmarkStackEvent.value = testList
+            mainRepository.getBooksHome()
+                .onSuccess {
+                    _bookmarkStackEvent.value = it
+
+            }.onError {
+
+            }
         }
     }
 }
