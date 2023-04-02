a4c665120274490e0540563c9767f5db69c4e597
junjange <wnswkd486@naver.com>
2023-04-02 12:34:38 +0900

    [CheckBookmark] 책 생성하기 > 책 생성 API 연결

--- app/src/main/java/com/o2/check_bookmark_android/ui/bookcreate/BookCreateViewModel.kt
+++ app/src/main/java/com/o2/check_bookmark_android/ui/bookcreate/BookCreateViewModel.kt
@@ -1,6 +1,10 @@
 package com.o2.check_bookmark_android.ui.bookcreate
 
 import com.o2.check_bookmark_android.base.BaseViewModel
+import com.o2.data.model.error.InvalidAccessTokenException
+import com.o2.domain.onError
+import com.o2.domain.onSuccess
+import com.o2.domain.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,6 +15,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BookCreateViewModel @Inject constructor(
+    private val mainRepository: MainRepository
 ) : BaseViewModel(), BookCreateActionHandler {
 
     private val TAG = "BookCreateViewModel"
@@ -29,23 +34,21 @@ class BookCreateViewModel @Inject constructor(
     override fun onBookCreateClicked() {
         if (titleEvent.value != "" && authorEvent.value != "" && publisherEvent.value != "" && totalPagesEvent.value != "") {
             baseViewModelScope.launch {
-//                mainRepository.postNotificationReservation(
-//                    group_id = groupId.value,
-//                    title = editTextTitleEvent.value,
-//                    content = editTextMessageEvent.value,
-//                    image_url = messageImgUri.value,
-//                    send_at = sendAt,
-//                ).onSuccess {
+                mainRepository.postBooks(
+                    bookName = titleEvent.value,
+                    author = authorEvent.value,
+                    publisher = publisherEvent.value,
+                    pageNumber = totalPagesEvent.value,
+                ).onSuccess {
                     _navigationEvent.emit(BookCreateNavigationAction.NavigateToBooks(1))
-//                }.onError {
-//                    Log.d("ttt 예약 알림 보내기 실패", it.toString())
-//                    when (it) {
+                }.onError {
+                    when (it) {
 //                        // 이미 예약했다면
 //                        is InvalidAccessTokenException -> _navigationEvent.emit(
 //                            AlarmCreateNavigationAction.NavigateToNoReservationAlarm
 //                        )
-//                    }
-//                }
+                    }
+                }
             }
         }
     }
